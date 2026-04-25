644d822e91c341a21ceb2e6af9c52135ca24bbb8
kstafa <kejsistafa2001@gmail.com>
2026-04-26 01:05:04 +0200

    Project init

---
+++ src/test/java/crm/backend/car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package crm.backend.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
